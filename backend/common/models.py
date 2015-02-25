800593d36bb0dba720f673607fcb6ed8d530582c
Liyosi Collins <collinskivale@gmail.com>
2015-02-25 04:05:06 +0300

    added job model

--- backend/common/models.py
+++ backend/common/models.py
@@ -19,3 +19,19 @@ class Industry(AbstractBase):
 
 class Skill(AbstractBase):
     name = models.CharField(max_length=128, null=False, blank=False)
+
+
+class Job(AbstractBase):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    start_date = models.DateTimeField(default=timezone.now)
+    expires_on = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    industry = models.ForeignKey(Industry, related_name='industry')
+    skills = models.ManyToManyField(Skill, related_name='skills')
+    location = models.ForeignKey(Location)
+
+    """
+        Todo:
+        validate expires_on > start_date
+        automatically set is_active=False when now()==expires_on
+    """
